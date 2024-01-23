33520365c9f26465d0b5be78d82911d66dda44b3
xyclose11 <harrisgus1@hotmail.com>
2024-01-23 17:19:48 -0600

    Initial Commit & Push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,257 +1,107 @@
-import Head from "next/head";
-import clientPromise from "../lib/mongodb";
-import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import Layout from '../components/Layout';
+import { useState } from 'react';
 
-type ConnectionStatus = {
-  isConnected: boolean;
-};
+type Props = {
+  posts: [Post]
+}
 
-export const getServerSideProps: GetServerSideProps<
-  ConnectionStatus
-> = async () => {
+type Post = {
+  _id: String;
+  title: String;
+  content: String;
+}
+
+export async function getServerSideProps() {
   try {
-    await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
+    let response = await fetch('http://localhost:3000/api/getPosts');
+    let posts = await response.json();
 
     return {
-      props: { isConnected: true },
+      props: { posts: JSON.parse(JSON.stringify(posts)) },
     };
   } catch (e) {
     console.error(e);
-    return {
-      props: { isConnected: false },
-    };
   }
-};
+}
 
-export default function Home({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+
+export default function Posts(props: Props) {
+
+  const [posts, setPosts] = useState<[Post]>(props.posts);
+
+  const handleDeletePost = async (postId: string) => {
+    try {
+      let response = await fetch('http://localhost:3000/api/deletePost?id=' + postId, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      });
+      response = await response.json();
+      window.location.reload();
+    } catch (error) {
+      console.log('An error occurred while deleting ', error);
+    }
+  }
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
-
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
-            for instructions.
-          </h2>
-        )}
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+    <Layout>
+      <div className="posts-body">
+        <h1 className="posts-body-heading">Top 20 Added Posts</h1>
+        {
+          posts.length > 0 ? (
+            <ul className="posts-list">
+              {posts.map((post, index) => {
+                return (
+                  <li key={index} className="post-item">
+                    <div className="post-item-details">
+                      <h2>{post.title}</h2>
+                      <p>{post.content}</p>
+                    </div>
+                    <div className="post-item-actions">
+                      <a href={`/posts/${post._id} `}>Edit</a>
+                      <button onClick={() => handleDeletePost(post._id as string)}>Delete</button>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          ) : (
+            <h2 className="posts-body-heading">Ooops! No posts added so far</h2>
+          )
         }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+      </div>
+      <style jsx>
+        {
+          `
+        .posts-body{
+            width:400px;
+            margin:10px auto;
         }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .posts-body-heading{
+            font-family:sans-serif;
         }
-
-        footer img {
-          margin-left: 0.5rem;
+        .posts-list{
+            list-style-type:none;
+            display:block;
         }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .post-item{
+            width:100%;
+            padding:10px;
+            border: 1px solid #d5d5d5;
         }
-
-        a {
-          color: inherit;
-          text-decoration: none;
+        .post-item-actions{
+            display:flex;
+            justify-content:space-between;
         }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
+        .post-item-actions a{
+            text-decoration:none;
         }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
+        `
         }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .subtitle {
-          font-size: 2rem;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      </style>
+    </Layout>
   );
 }
